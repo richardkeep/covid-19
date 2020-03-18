bdf519bbd5326e51dbc5ccc6ceb1b00da868bf03
Richard Keep <r.kipsang@gmail.com>
2020-03-19 00:38:25 +0300

    Use my own api

--- app/Corona.php
+++ app/Corona.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use Sushi\Sushi;
-use Goutte\Client;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
@@ -35,44 +34,11 @@ class Corona extends Model
         'Faeroe Islands' => 'Faroe Islands',
     ];
 
-    protected $casts = [
-        'todayCases' => 'integer',
-        'todayDeaths' => 'integer',
-        'cases' => 'integer',
-        'deaths'    => 'integer',
-        'recovered' => 'integer',
-        'activeCases'   => 'integer',
-        'critical' => 'integer',
-    ];
-
     public function getRows()
     {
-        $client = new Client();
-        $crawler = $client->request('GET', 'https://www.worldometers.info/coronavirus/');
-
-        $keys = ['country', 'cases', 'todayCases', 'deaths', 'todayDeaths', 'recovered', 'activeCases', 'critical'];
-
-        return collect($crawler->filter('#main_table_countries_today')->filter('tr')->each(function ($tr, $i) {
-            return $tr->filter('td')->each(function ($td, $j) {
-                return trim($td->text());
-            });
-        }))->filter(function ($item, $key) {
-            return $key > 0;
-        })->map(function ($item, $k) use ($keys) {
-            foreach ($keys as $kk => $v) {
-                if ($k > 0) {
-                    $d[$v] = str_replace(',', '', $item[$kk]);
-                } else {
-                    $d[$v] = $item[$kk];
-                }
-            }
-
-            return $d;
-        })->values()->all();
-
-        // return Cache::remember('corona', Carbon::parse('10 minutes'), function () {
-        //     return Http::get('https://corona.lmao.ninja/countries')->json();
-        // });
+        return Cache::remember('covid19', Carbon::parse('10 minutes'), function () {
+            return Http::get('https://corona.richardkeep.dev/countries')->json();
+        });
     }
 
     public function emoji()
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,3 @@
 <?php
 
-use App\Corona;
-
-Route::get('/data', function () {
-    return Corona::all();
-});
-
 Route::view('/', 'corona');
